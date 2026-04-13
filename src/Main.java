708d91ed6130c67d84eeb173e28400ece6c7125a
sm150820 <sm150820@student.sgh.waw.pl>
2026-04-13 14:49:39 +0000

    LAST UPDATE

--- src/Main.java
+++ src/Main.java
@@ -25,28 +25,28 @@ public class Main {
         FinancialReport.Report(redTech); 
      
 
-        // // Compare profit 
-        // if (blueSoft.calculateProfit() > redTech.calculateProfit()) {
-        //     System.out.println("BlueSoft is more profitable than RedTech.");
-        // } else if (redTech.calculateProfit() > blueSoft.calculateProfit()) {
-        //     System.out.println("RedTech is more profitable than BlueSoft.");
-        // } else {
-        //     System.out.println("Both companies have the same profit.");
-        // }
+        // Compare profit 
+        if (blueSoft.calculateProfit() > redTech.calculateProfit()) {
+            System.out.println("BlueSoft is more profitable than RedTech.");
+        } else if (redTech.calculateProfit() > blueSoft.calculateProfit()) {
+            System.out.println("RedTech is more profitable than BlueSoft.");
+        } else {
+            System.out.println("Both companies have the same profit.");
+        }
 
-        // System.out.println();
+        System.out.println();
 
     
-        // if (!blueSoft.canOperate()) {
-        //     System.out.println("BlueSoft has gone bankrupt. GAME OVER.");
-        // } else {
-        //     System.out.println("BlueSoft can continue operating. NEXT TURN.");
-        // }
+        if (!blueSoft.canOperate()) {
+            System.out.println("BlueSoft has gone bankrupt. GAME OVER.");
+        } else {
+            System.out.println("BlueSoft can continue operating. NEXT TURN.");
+        }
 
-        // if (!redTech.canOperate()) {
-        //     System.out.println("RedTech has gone bankrupt. GAME OVER.");
-        // } else {
-        //     System.out.println("RedTech can continue operating. NEXT TURN.");
-        // }
+        if (!redTech.canOperate()) {
+            System.out.println("RedTech has gone bankrupt. GAME OVER.");
+        } else {
+            System.out.println("RedTech can continue operating. NEXT TURN.");
+        }
     }
 }
